6b36acab66a566aa8110297f97b90d016cb18c4c
jterrace <jterrace@41643c41-6115-4a67-97d8-b2dbeab842be>
2008-09-27 17:35:50 +0000

    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@31 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ Node.cpp
@@ -0,0 +1,43 @@
+#include "Node.h"
+
+Node::Node() {}
+
+Node::~Node() {}
+
+Node::Node(rpc_node newnode) {
+	set_from_rpc_node(newnode);
+}
+Node::Node(string newip, unsigned int newport, ID_Value newid) :
+	ip(newip), port(newport), id(newid) {}
+
+rpc_node Node::get_rpc_node() {
+	rpc_node ret;
+	ret.ip = ip.c_str();
+	ret.id = id.get_rpc_id();
+	ret.port = port;
+	return ret;
+}
+
+void Node::set_from_rpc_node(rpc_node newnode) {
+	ip = newnode.ip;
+	ID_Value newid(newnode.id);
+	id = newid;
+	port = newnode.port;
+}
+
+const string Node::toString() const {
+	ostringstream ss;
+	ss << "(" << ip
+	   << "," << port
+	   << "," << id.toString()
+	   << ")";
+	return ss.str();
+}
+
+string Node::getIp() { return ip; }
+unsigned int Node::getPort() { return port; }
+ID_Value Node::getId() { return id; }
+
+void Node::setIp(string newip) { ip = newip; }
+void Node::setPort(unsigned int newport) { port = newport; }
+void Node::setId(ID_Value newid) { id = newid; }
