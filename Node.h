657461867cd4ffd3826b89c3576e8e2a16637c79
jterrace <jterrace@41643c41-6115-4a67-97d8-b2dbeab842be>
2008-09-27 17:35:55 +0000

    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@32 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ Node.h
@@ -0,0 +1,38 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+#include <sstream>
+#include <string>
+#include "craq_rpc.h"
+#include "ID_Value.h"
+
+using namespace std;
+
+class Node
+{
+private:
+	string ip;
+	unsigned int port;
+	ID_Value id;
+	
+public:
+	Node();
+	Node(rpc_node newnode);
+	Node(string newip, unsigned int newport, ID_Value newid);
+	virtual ~Node();
+	
+	rpc_node get_rpc_node();
+	void set_from_rpc_node(rpc_node newnode);
+	
+	const string toString() const;
+	
+	string getIp();
+	unsigned int getPort();
+	ID_Value getId();
+	
+	void setIp(string newip);
+	void setPort(unsigned int newport);
+	void setId(ID_Value newid);
+};
+
+#endif /*NODE_H_*/
