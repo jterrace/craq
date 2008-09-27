a89c73d348d8155f85b9b088c17e05570600dc22
jterrace <jterrace@41643c41-6115-4a67-97d8-b2dbeab842be>
2008-09-27 17:35:41 +0000

    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@29 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ MemStorage.cpp
@@ -0,0 +1,53 @@
+#include "MemStorage.h"
+
+MemStorage::MemStorage()
+{
+}
+
+MemStorage::~MemStorage()
+{
+}
+
+const blob* MemStorage::get(ID_Value key) {
+	map<ID_Value, blob*>::iterator it = mem_data.find(key);
+	if(it == mem_data.end()) {
+		return NULL;
+	} else {
+		return it->second;
+	}
+}
+
+bool MemStorage::set(ID_Value key, const blob* data) {
+	mem_data[key] = new blob(*data);
+	return true;
+}
+
+bool MemStorage::add(ID_Value key, const blob* data) {
+	map<ID_Value, blob*>::iterator it = mem_data.find(key);
+	if(it == mem_data.end()) {
+		mem_data[key] = new blob(*data);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool MemStorage::replace(ID_Value key, const blob* data) {
+	map<ID_Value, blob*>::iterator it = mem_data.find(key);
+	if(it == mem_data.end()) {
+		return false;
+	} else {
+		mem_data[key] = new blob(*data);
+		return true;
+	}
+}
+
+
+bool MemStorage::del(ID_Value key) {
+	map<ID_Value, blob*>::iterator it = mem_data.find(key);
+	if(it != mem_data.end()) {
+		delete it->second;
+		mem_data.erase(key);
+	}
+	return true;
+}
