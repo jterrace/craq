da2c4f99add0bec4e7f8317c40e01d65d8490429
jterrace <jterrace@41643c41-6115-4a67-97d8-b2dbeab842be>
2009-01-09 08:18:45 +0000

    udp
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@438 41643c41-6115-4a67-97d8-b2dbeab842be

--- connection_pool.h
+++ connection_pool.h
@@ -18,18 +18,18 @@ struct conn_info {
 	unsigned int port;
 	int fd;
 	const rpc_program * prog;
-	ptr<axprt_stream> x;
+	ptr<axprt_dgram> x;
 	ptr<aclnt> cli;
 };
 
 void
-get_rpc_cli( const char * host, 
-				unsigned int port, 
+get_rpc_cli( const char * host,
+				unsigned int port,
 				ptr<aclnt> * call_ret,
-				const rpc_program * prog, 
+				const rpc_program * prog,
 				evi_t ev,
 				CLOSURE );
-				
+
 void
 invalidate_rpc_host( const char * host,
 				unsigned int port );
