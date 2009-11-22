ba7c8be8d1eeefe954072f9d555cc34b325673d7
patrickw <patrickw@41643c41-6115-4a67-97d8-b2dbeab842be>
2009-11-22 23:03:32 +0000

    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@2116 41643c41-6115-4a67-97d8-b2dbeab842be

--- donar/resolver.h
+++ donar/resolver.h
@@ -1,41 +1,71 @@
 #include <tr1/unordered_map>
 #include <tr1/unordered_set>
+#include <iostream>
+#include "tame.h"
+#include <string>
+#include <utility>
+#include "donar_rpc.h"
+#include <vector>
 
 using namespace std::tr1;
+using std::vector;
+using std::pair;
 
 // Storage class for recordkeeping the number of requests
 // from a given client region. The region is defined in terms
 // of a lat/lon pair. The decision of how to map clients into
 // a client region is not handled in this class.
 class ClientRegion {
-  private:
-    double lat;
-    double lon;
-
   public:
-    ClientRegion(double lat, double lon);
+    int lat, lon;
+    ClientRegion();
+    ClientRegion(int lat, int lon);
     bool operator==(const ClientRegion &other);
     bool operator>(const ClientRegion &other);    
     bool operator<(const ClientRegion &other);
     int get_hash();
 };
 
-// Local info stored per record
-class RecordInfo {
+class ClientRegionEqual {
   public:
-    // Answer rule table, populated by optimization
-    unordered_map<ClientRegion*, float> probs;
+    size_t operator()(const ClientRegion &cr1, const ClientRegion &cr2) const;
 };
+
+class ClientRegionHash {
+  public:
+    size_t operator()(const ClientRegion &cr) const;
+};
+
+typedef callback<void, ClientRegion>::ref cb_cr;
 
 // Local information stored per subdomain
 class SubdomainInfo {
   public:
-    int seq_num;
     time_t validated; // How stale
+    subdomain storage; // CRAQ storage struct
 
-    // Maps a given minute (key is the first second in that min.)
-    // to the number of requests received.
-    unordered_map<unsigned int, unsigned int> num_requests;
+    // For each client region, maps a time value to a request volume.
+    unordered_map<ClientRegion, unordered_map<size_t, unsigned int>, 
+      ClientRegionHash, ClientRegionEqual> num_requests;
 
-    unordered_set<RecordInfo> records;
+    // For each record, for each client region, store the probability
+    // of routing to that client region.
+    vector<pair<record, unordered_map<ClientRegion*, float> > > rules;
 };
+
+typedef unordered_map<ClientRegion, unordered_map<size_t, unsigned int>, 
+      ClientRegionHash, ClientRegionEqual>::iterator num_requests_iter;
+
+class SubdomainEqual {
+  public:
+    size_t operator()(const SubdomainInfo &s1, const SubdomainInfo &s2) const;
+};
+
+class SubdomainHash {
+  public:
+    size_t operator()(const SubdomainInfo &s) const;
+};
+
+typedef callback<void, SubdomainInfo*>::ref cb_sub;
+
+//void assure_in_cache(string fqdn, CLOSURE);
