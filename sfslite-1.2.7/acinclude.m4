bf3faa8f4408ae26d352d2ea337252f6a0c1dfb4
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-03-27 04:28:16 +0000

    removing sfslite in preparation for a more correct version
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@3943 41643c41-6115-4a67-97d8-b2dbeab842be

--- sfslite-1.2.7/acinclude.m4
+++
@@ -1,2 +0,0 @@
-dnl $Id: acinclude.m4 2063 2006-06-28 18:23:39Z max $
-m4_include(acsfs.m4)
--- sfslite-1.2.7/libtame/trigger.C
+++
@@ -1,9 +0,0 @@
-
-#include "tame_trigger.h"
-
-void
-dtrigger (event<>::ref cb)
-{
-  delaycb (0, 0, wrap (cb, &_event<>::trigger));
-}
-
