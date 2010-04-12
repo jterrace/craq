66df2a4e8fd3c861fe9f6952c8ad4cc3ad8448fa
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-12 07:05:15 +0000

    here's the version that kina works
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4245 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ lighttpd_files/db.pl
@@ -0,0 +1,39 @@
+#!/opt/local/bin/perl
+
+use BerkeleyDB ;
+
+print "Content-type: text/plain\n\n";
+
+$request_method = $ENV{'REQUEST_METHOD'};
+$request_uri = substr($ENV{'REQUEST_URI'}, 1);
+$content_length = $ENV{'CONTENT_LENGTH'};
+
+my $filename = "database";
+my $errorlog = "dberror";
+
+if ($request_method eq 'GET') {
+
+	$db = new BerkeleyDB::Hash( -Filename => $filename,
+				    -Flags => DB_RDONLY) or die "Cannot open file $filename";
+	$db->db_get($request_uri, $val);
+	print $val;	
+	
+} elsif ($request_method eq 'PUT') {
+	
+	read(STDIN, $buffer, $content_length);
+	if (!(-e $filename)) {
+		$db = new BerkeleyDB::Hash( -Filename => $filename,
+					    -Flags => DB_CREATE) or die "Cannot open file $filename";
+        } else {
+		$db = new BerkeleyDB::Hash( -Filename => $filename) or die "Cannot open file $filename";
+	}
+	$db->db_put($request_uri, $buffer);
+	
+} elsif ($request_method eq 'DELETE') {
+
+	$db = new BerkeleyDB::Hash( -Filename => $filename) or die "Cannot open file $filename";
+        $db->db_del($request_uri);
+	
+} else {
+	exec('echo ' . '"unrecognized request method: ' . $request_method . '" > ' . $errorlog);
+}
