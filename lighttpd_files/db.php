0effb3c0d367b020c8518a6dfaef79bffc96079e
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-13 09:15:37 +0000

    moved to a php implementation of http storage because it's much faster
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4255 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ lighttpd_files/db.php
@@ -0,0 +1,32 @@
+<?php
+
+$request_method = $_ENV['REQUEST_METHOD'];
+$request_uri = substr($_ENV['REQUEST_URI'], 1);
+$content_length = $_ENV['CONTENT_LENGTH'];
+
+$filename = "database";
+
+$db = new Db4();
+
+if ($request_method === 'GET') {
+
+	$db->open(NULL, $filename, NULL);
+	$val = $db->get($request_uri);
+	echo $val;
+        
+} else if ($request_method === 'PUT') {
+        
+	$buffer = file_get_contents('php://input');
+	$db->open(NULL, $filename, NULL, DB_CREATE);
+	$db->put($request_uri, $buffer);
+	
+} else if ($request_method === 'DELETE') {
+
+	$db->open(NULL, $filename, NULL);
+	$db->del($request_uri);
+
+}
+
+$db->close();
+
+?>
