006f3d9bfabbc49f998f84aa053f19d7273211f4
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 05:37:12 +0000

    database using persistent connection
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4280 41643c41-6115-4a67-97d8-b2dbeab842be

---
+++ lighttpd_files/db2.php
@@ -0,0 +1,28 @@
+<?php
+
+$request_method = $_ENV['REQUEST_METHOD'];
+$request_uri = substr($_ENV['REQUEST_URI'], 1);
+$content_length = $_ENV['CONTENT_LENGTH'];
+
+$filename = "/tmp/data.db";
+
+if ($request_method === 'GET') {
+
+	$id = dba_popen($filename, "r", "gdbm");
+	$val = dba_fetch($request_uri, $id);
+	echo $val;
+        
+} else if ($request_method === 'PUT') {
+        
+	$buffer = file_get_contents('php://input');
+	$id = dba_popen($filename, "c", "gdbm");
+	dba_insert($request_uri, $buffer, $id);
+	
+} else if ($request_method === 'DELETE') {
+
+	$id = dba_popen($filename, "w", "gdbm");
+	dba_delete($request_uri, $id);
+
+}
+
+?>
