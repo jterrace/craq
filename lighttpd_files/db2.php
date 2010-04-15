d4af95cfa8632e7809dc1f6c6f7125f33307380a
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 08:09:35 +0000

    trying a global variable with popen
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4287 41643c41-6115-4a67-97d8-b2dbeab842be

--- lighttpd_files/db2.php
+++ lighttpd_files/db2.php
@@ -1,5 +1,7 @@
 <?php
 
+global $id;
+
 $request_method = $_ENV['REQUEST_METHOD'];
 $request_uri = substr($_ENV['REQUEST_URI'], 1);
 $content_length = $_ENV['CONTENT_LENGTH'];
@@ -8,19 +10,19 @@ $filename = "/tmp/data.db";
 
 if ($request_method === 'GET') {
 
-	$id = dba_open($filename, "r", "gdbm");
+	$id = dba_popen($filename, "r", "gdbm");
 	$val = dba_fetch($request_uri, $id);
 	echo $val;
 
 } else if ($request_method === 'PUT') {
         
 	$buffer = file_get_contents('php://input');
-	$id = dba_open($filename, "c", "gdbm");
+	$id = dba_popen($filename, "c", "gdbm");
 	dba_replace($request_uri, $buffer, $id);	
 	
 } else if ($request_method === 'DELETE') {
 
-	$id = dba_open($filename, "w", "gdbm");
+	$id = dba_popen($filename, "w", "gdbm");
 	dba_delete($request_uri, $id);
 
 }
