9d40710d606de55f07499c0e0c5c78bc911c27cd
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 06:05:43 +0000

    testing something with cdb, this version shouldn't acutally be used
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4282 41643c41-6115-4a67-97d8-b2dbeab842be

--- lighttpd_files/db2.php
+++ lighttpd_files/db2.php
@@ -8,20 +8,20 @@ $filename = "/tmp/data.db";
 
 if ($request_method === 'GET') {
 
-	$id = dba_popen($filename, "r", "gdbm");
+	$id = dba_popen($filename, "r", "cdb");
 	$val = dba_fetch($request_uri, $id);
 	echo $val;
         
 } else if ($request_method === 'PUT') {
         
 	$buffer = file_get_contents('php://input');
-	$id = dba_popen($filename, "c", "gdbm");
-	dba_insert($request_uri, $buffer, $id);
+	#$id = dba_popen($filename, "c", "gdbm");
+	#dba_insert($request_uri, $buffer, $id);
 	
 } else if ($request_method === 'DELETE') {
 
-	$id = dba_popen($filename, "w", "gdbm");
-	dba_delete($request_uri, $id);
+	#$id = dba_popen($filename, "w", "gdbm");
+	#dba_delete($request_uri, $id);
 
 }
 
