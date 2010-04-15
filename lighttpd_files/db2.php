57c99828e4fcec02bfc08b59b785a40368cd9c17
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 07:55:22 +0000

    removed session stuff
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4286 41643c41-6115-4a67-97d8-b2dbeab842be

--- lighttpd_files/db2.php
+++ lighttpd_files/db2.php
@@ -1,14 +1,5 @@
 <?php
 
-session_name("s");
-session_start();
-
-if (empty($_SESSION['count'])) {
-        $_SESSION['count'] = 1;
-} else {
-        $_SESSION['count']++;
-}
-
 $request_method = $_ENV['REQUEST_METHOD'];
 $request_uri = substr($_ENV['REQUEST_URI'], 1);
 $content_length = $_ENV['CONTENT_LENGTH'];
@@ -17,33 +8,21 @@ $filename = "/tmp/data.db";
 
 if ($request_method === 'GET') {
 
-	if (!empty($_SESSION[$request_uri])) {
-                echo $_SESSION[$request_uri];
-        } else {
-		$id = dba_open($filename, "r", "gdbm");
-		$val = dba_fetch($request_uri, $id);
-		echo $val;
-	}        
+	$id = dba_open($filename, "r", "gdbm");
+	$val = dba_fetch($request_uri, $id);
+	echo $val;
 
 } else if ($request_method === 'PUT') {
         
 	$buffer = file_get_contents('php://input');
-        $_SESSION[$request_uri] = $buffer;
+	$id = dba_open($filename, "c", "gdbm");
+	dba_replace($request_uri, $buffer, $id);	
 	
 } else if ($request_method === 'DELETE') {
 
-	unset($_SESSION[$request_uri]);
 	$id = dba_open($filename, "w", "gdbm");
 	dba_delete($request_uri, $id);
 
-}
-
-if ($_SESSION['count'] > 1000000) {
-        unset($_SESSION['count']);
-        $db->open(NULL, $filename, NULL, DB_CREATE);
-        foreach ($_SESSION as $key => $value) {
-                $db->put($key, $value);
-        }
 }
 
 ?>
