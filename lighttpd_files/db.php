15066461724b0f793e31cb6fcbb977a4a2926ee8
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 07:20:17 +0000

    now uses sessions to keep data in memory
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4284 41643c41-6115-4a67-97d8-b2dbeab842be

--- lighttpd_files/db.php
+++ lighttpd_files/db.php
@@ -1,5 +1,14 @@
 <?php
 
+session_name("s");
+session_start();
+
+if (empty($_SESSION['count'])) {
+	$_SESSION['count'] = 1;
+} else {
+	$_SESSION['count']++;
+}
+
 $request_method = $_ENV['REQUEST_METHOD'];
 $request_uri = substr($_ENV['REQUEST_URI'], 1);
 $content_length = $_ENV['CONTENT_LENGTH'];
@@ -10,21 +19,33 @@ $db = new Db4();
 
 if ($request_method === 'GET') {
 
-	$db->open(NULL, $filename, NULL);
-	$val = $db->get($request_uri);
-	echo $val;
+	if (!empty($_SESSION[$request_uri])) {
+		echo $_SESSION[$request_uri];
+	} else {
+		$db->open(NULL, $filename, NULL);
+		$val = $db->get($request_uri);
+		echo $val;
+	}
         
 } else if ($request_method === 'PUT') {
         
 	$buffer = file_get_contents('php://input');
-	$db->open(NULL, $filename, NULL, DB_CREATE);
-	$db->put($request_uri, $buffer);
+	$_SESSION[$request_uri] = $buffer;
 	
 } else if ($request_method === 'DELETE') {
 
+	unset($_SESSION[$request_uri]);
 	$db->open(NULL, $filename, NULL);
 	$db->del($request_uri);
 
+}
+
+if ($_SESSION['count'] > 100) {
+	unset($_SESSION['count']);
+	$db->open(NULL, $filename, NULL, DB_CREATE);
+	foreach ($_SESSION as $key => $value) {
+        	$db->put($key, $value);
+	}
 }
 
 ?>
