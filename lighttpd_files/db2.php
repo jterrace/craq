e952377fdd6d201690d15549de131387be007e42
rucinski <rucinski@41643c41-6115-4a67-97d8-b2dbeab842be>
2010-04-15 06:16:54 +0000

    now only uses open instead of popen
    git-svn-id: https://adam.cs.princeton.edu/svn/craq/src@4283 41643c41-6115-4a67-97d8-b2dbeab842be

--- lighttpd_files/db2.php
+++ lighttpd_files/db2.php
@@ -8,20 +8,20 @@ $filename = "/tmp/data.db";
 
 if ($request_method === 'GET') {
 
-	$id = dba_popen($filename, "r", "cdb");
+	$id = dba_open($filename, "r", "gdbm");
 	$val = dba_fetch($request_uri, $id);
 	echo $val;
         
 } else if ($request_method === 'PUT') {
         
 	$buffer = file_get_contents('php://input');
-	#$id = dba_popen($filename, "c", "gdbm");
-	#dba_insert($request_uri, $buffer, $id);
+	$id = dba_open($filename, "c", "gdbm");
+	dba_insert($request_uri, $buffer, $id);
 	
 } else if ($request_method === 'DELETE') {
 
-	#$id = dba_popen($filename, "w", "gdbm");
-	#dba_delete($request_uri, $id);
+	$id = dba_open($filename, "w", "gdbm");
+	dba_delete($request_uri, $id);
 
 }
 
